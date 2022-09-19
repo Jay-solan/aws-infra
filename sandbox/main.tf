2b7d385df0c786a74a1dfc1e13e43c72195cba3f
Jaipal Solanki <jaipalsolanki101@gmail.com>
2022-09-19 21:32:46 +0530

    Updated main.tf

--- sandbox/main.tf
+++ sandbox/main.tf
@@ -1,18 +1,18 @@
 locals {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 # terraform configurations
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.20.1"
+      # version = "4.20.1"
     }
   }
   backend "s3" {
-    bucket = "615098137319-tfstate"
+    bucket = "tmp-tst-elastic-snap"
     key    = "dev/sandbox/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
 
@@ -23,7 +23,8 @@ provider "aws" {
 
 # calling tags module
 module "tags" {
-  source = "git@github.com:Jay-solan/aws-infra-module.git//tags?ref=main"
+  # source = "git@github.com:Jay-solan/aws-infra-module.git//tags?ref=main"
+  source = "../../aws-infra-module/tags"
 
   names = ["app-name", "dev", "frontend"]
 
@@ -37,7 +38,8 @@ module "tags" {
 # creating ecr
 
 module "ecr" {
-  source = "git@github.com:Jay-solan/aws-infra-module.git//ecr?ref=main"
+  # source = "git@github.com:Jay-solan/aws-infra-module.git//ecr?ref=main"
+  source = "../../aws-infra-module/ecr"
   name   = module.tags.name
   tags   = module.tags.tags
 
@@ -47,7 +49,8 @@ module "ecr" {
 
 
 module "infra" {
-  source = "git@github.com:Jay-solan/aws-infra-module.git//environment?ref=main"
+  # source = "git@github.com:Jay-solan/aws-infra-module.git//environment?ref=main"
+  source = "../../aws-infra-module/environment"
   name   = module.tags.name
   name6  = module.tags.name6
   tags   = module.tags.tags
@@ -55,7 +58,7 @@ module "infra" {
 
   // VPC
   cidr            = "10.0.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
